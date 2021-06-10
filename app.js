b65a7b4f282a95aec277ded9317518e3aff70522
Javier Martinez Ribo <javiermribo@gmail.com>
2021-06-10 14:14:29 -0300

    event listener added to form. Fn created to push items to activitiesArrayList

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const ACTIVITIES_LIST_SELECTOR = document.querySelector("#activitiesList");
 const SUBMIT_BTN_SELECTOR = document.querySelector("#taskToListBtnId");
 const INPUT_FIELD_SELECTOR = document.querySelector("#taskInputFieldId");
 
+eventsListeners();
+
+function eventsListeners () {
+  FORM_SELECTOR.addEventListener('submit', itemToArray);
+}
+
 let activitiesArrayList = [];
+
 
 const newActivityInput = (activity) => {
   const activityItem = {
@@ -13,4 +20,11 @@ const newActivityInput = (activity) => {
   }
   activitiesArrayList.push(activityItem);
   return activityItem;
+}
+
+function itemToArray (event) {
+  event.preventDefault();
+  //const FORM_SELECTOR = document.querySelector("#newTaskFormId");
+  const activity = INPUT_FIELD_SELECTOR.value;
+  newActivityInput(activity);
 }
